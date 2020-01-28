d746acd8d420bab3be22af6299fae161fcf3bb58
Rathna Ganjigunta <rathna.ganjigunta@gmail.com>
2020-01-28 13:54:51 -0800

    added thunk and handled async actions by adding another reducer

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -4,34 +4,39 @@ import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox';
 import ErrorBoundry from '../Components/ErrorBoundry';
 import './App.css';
-import {setSearchField} from '../actions';
+import {setSearchField, requestRobots} from '../actions';
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
 }
 
 const mapDispatchToProps = (dispatch) =>{
   return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
   }
 }
 
 class App extends Component{
-  constructor(){
-    super();
-    this.state = {
-      robots: [],
-      //searchField: ''
-    }
-  }
+  // constructor(){
+  //   super();
+  //   this.state = {
+  //     robots: [],
+  //     //searchField: ''
+  //   }
+  // }
 
   componentDidMount() {
+    this.props.onRequestRobots();
 
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users}));
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then(response => response.json())
+    //   .then(users => this.setState({ robots: users}));
   }
 
   // onSearchChange = (event) =>{
@@ -39,13 +44,12 @@ class App extends Component{
   // }
 
   render() {
-    const {robots} = this.state;
-    const {searchField, onSearchChange} = this.props;
+    const {searchField, onSearchChange, robots, isPending} = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    return !robots.length ?
+    return isPending ?
        <h1>Loading...</h1> :
        (
         <div className='tc'>
